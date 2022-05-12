ca2ebbbf5c022f2f2bc085bfea67cdf3984d1b41
Nikita Yashin <n.yashin88@icloud.com>
2022-05-12 11:12:51 +0300

    Initial Commit

---
+++ RGBTrafficLight/RGBTrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBTrafficLightApp.swift
+//  RGBTrafficLight
+//
+//  Created by Nikita Yashin on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBTrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
