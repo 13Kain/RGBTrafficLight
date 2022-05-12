34090fa083ec410c778a1c76ebe6326a16bd8795
Nikita Yashin <n.yashin88@icloud.com>
2022-05-12 15:46:06 +0300

    problems with button

--- RGBTrafficLight/ContentView.swift
+++ RGBTrafficLight/ContentView.swift
@@ -9,23 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Button(<#T##titleKey: LocalizedStringKey##LocalizedStringKey#>, action: <#T##() -> Void#>)
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all)
-            VStack {
+            VStack(spacing: 16) {
                 Circle()
                     .foregroundColor(.red)
                     .frame(width: 150, height: 150)
-                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                    .opacity(0.35)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
                 Circle()
                     .foregroundColor(.yellow)
                     .frame(width: 150, height: 150)
+                    .opacity(0.35)
                     .overlay(Circle().stroke(Color.white, lineWidth: 4))
                 Circle()
                     .foregroundColor(.green)
                     .frame(width: 150, height: 150)
+                    .opacity(0.35)
                     .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Spacer()
             }
-            .padding(.top)
+            .padding(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
         }
     }
 }
